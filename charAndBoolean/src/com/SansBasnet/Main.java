7112981cabd4a8d373192a6dd048b481d506dd3c
Sans Basnet <43323395+SansBasnet@users.noreply.github.com>
2020-06-26 10:14:59 -0500

    Add files via upload

---
+++ charAndBoolean/src/com/SansBasnet/Main.java
@@ -0,0 +1,19 @@
+package com.SansBasnet;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char myChar = 'D';
+        char myUniCode = '\u0044';
+        char myUniCodeChar = '\u00A9';
+        System.out.println(myChar);
+        System.out.println(myUniCodeChar);
+        System.out.println(myUniCode);
+
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+
+        boolean isCustomerOverTwentyOne = true;
+    }
+}

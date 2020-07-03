5056b824203aef1e5dafac7ef31c70f2c67f22d6
Sans Basnet <43323395+SansBasnet@users.noreply.github.com>
2020-07-03 11:32:10 -0500

    Add files via upload

---
+++ SpeedConverter/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        double miles = SpeedConveter.toMilesPerHour(kilometersPerHour: 10.5);
+        System.out.println("Miles = " + miles);
+    }
+}
